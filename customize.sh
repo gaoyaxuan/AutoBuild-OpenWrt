875ad3a6d6df765bd49f0ce09c40abb3956af0c5
高亚轩 <mail@gaoyaxuan.net>
2021-08-01 16:27:42 +0800

    默认还是使用uhttpd

--- customize.sh
+++ customize.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
-# 使用1.1.1j  1.1.1k会导致openssh 无法连接
+# 使用1.1.1j , 1.1.1k会导致openssh 无法连接
 sed -i 's/PKG_BUGFIX:=k/PKG_BUGFIX:=j/g' openwrt/package/libs/openssl/Makefile
+
+# 默认还是使用uhttpd
+rm -rf openwrt/feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+cp files/60_nginx-luci-support   openwrt/feeds/packages/net/nginx/files-luci-support/
