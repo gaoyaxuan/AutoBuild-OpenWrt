fe94dc601cb19ee9a160c73950a2956e59aa25fe
23333 <qmd3dtv4bkcwa7yd02fry@duck.com>
2021-08-01 16:01:03 +0800

    降级openssl的版本

--- customize.sh
+++ customize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
+# 使用1.1.1j  1.1.1k会导致openssh 无法连接
+sed -i 's/PKG_BUGFIX:=k/PKG_BUGFIX:=j/g' openwrt/package/libs/openssl/Makefile
