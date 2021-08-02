dac979407972ab4d68e307aab8cfeddb662abf05
高亚轩 <mail@gaoyaxuan.net>
2021-08-02 16:05:53 +0800

    custom

--- customize.sh
+++ customize.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
+cd openwrt
+
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 使用1.1.1j , 1.1.1k会导致openssh 无法连接
-sed -i 's/PKG_BUGFIX:=k/PKG_BUGFIX:=j/g' openwrt/package/libs/openssl/Makefile
+cp -f files/openssl-Makefile   package/libs/openssl/Makefile
+cp -f files/430-e_devcrypto-make-the-dev-crypto-engine-dynamic.patch  package/libs/openssl/patches/430-e_devcrypto-make-the-dev-crypto-engine-dynamic.patch
 
 # 默认还是使用uhttpd
-rm -rf openwrt/feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
-cp files/60_nginx-luci-support   openwrt/feeds/packages/net/nginx/files-luci-support/
+cp -f files/60_nginx-luci-support   feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+
+
+#openssh 更改PermitRootLogin yes
+code='sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" $(1)/etc/ssh/sshd_config'
+`sed -i "227a\${code}" feeds/packages/net/openssh/Makefile`
