1d5434743bd189967509b03c68b72060e71eddc9
23333 <qmd3dtv4bkcwa7yd02fry@duck.com>
2024-12-18 19:33:25 +0800

    update to luci23

--- diy-src.sh
+++ diy-src.sh
@@ -22,9 +22,9 @@ function git_sparse_clone() {
 
 
 # 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/msd_lite
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/packages/net/msd_lite
 #rm -rf feeds/packages/net/smartdns
 #rm -rf feeds/luci/themes/luci-theme-argon
 #rm -rf feeds/luci/themes/luci-theme-argon-mod
@@ -37,8 +37,8 @@ rm -rf feeds/luci/applications/luci-app-passwall2
 
 # 添加额外插件
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-#git clone --depth=1  https://github.com/gaoyaxuan/luci-app-pushbot package/luci-app-pushbot
+git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone --depth=1  https://github.com/gaoyaxuan/luci-app-pushbot package/luci-app-pushbot
 #git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
@@ -48,7 +48,8 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFil
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+#git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # msd_lite
 #git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
